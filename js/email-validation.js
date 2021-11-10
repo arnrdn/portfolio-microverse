549ceaae209207b42950db3eb39487f605778aef
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2021-11-10 12:13:32 +0100

    Implement custom error message for email validation

--- js/email-validation.js
+++ js/email-validation.js
@@ -1,1 +1,9 @@
 const emailInput = document.querySelector('#email');
+
+emailInput.addEventListener('input', () => {
+  if (emailInput.validity.patternMismatch) {
+    emailInput.setCustomValidity('Email must not contain uppercase characters');
+  } else {
+    emailInput.setCustomValidity('');
+  }
+});
