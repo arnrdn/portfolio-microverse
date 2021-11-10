25002790fc5dcf15f7c60d3cefe4d65e9ce6e5db
rdnrn <rodina.arina00@gmail.com>
2021-11-10 06:07:51 +0300

    Try

--- work-card-popup.js
+++ work-card-popup.js
@@ -1,9 +1,13 @@
-const openCardBtn = document.querySelector('#portfolio .work-btn');
-const workCard = document.querySelector('.work-overlay');
-const body1 = document.querySelector('body');
-const closeCardBtns = document.querySelectorAll('.work-close-btn');
+const workPopup = document.querySelector('.work-popup');
+const workOverlay = document.querySelector('.work-overlay');
+const workContent = document.querySelector('.work-content');
 const workCard1 = document.querySelector('#work-container-1');
 const workCardContainer = document.querySelector('#portfolio');
+const bodyCard = document.querySelector('body');
+const openCardBtn = document.querySelectorAll('.work-btn');
+const closeCardBtn = document.querySelectorAll('.work-close-btn');
+
+// object with info
 
 const workCards = [
   {
@@ -13,7 +17,7 @@ const workCards = [
       position: 'Back End Dev',
       year: 2015,
     },
-    imageSrc: './images/card-desktop-modal.png',
+    imageSrc: './images/work-example-d-1.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     tags: ['html', 'css', 'javaScript'],
     liveDemoLink: '#',
@@ -60,91 +64,47 @@ const workCards = [
   },
 ];
 
-
-
-function setImageSrc(imageSrc) {
-  const img = workCardContainer.querySelector('.work-img');
-  img.setAttribute('src', imageSrc);
-}
-
-function setDescription(description) {
-  const descriptionCard = workCardCopy.querySelector('.work-popup-p');
-  descriptionCard.textContent = description;
-};
-
-function setCompanyName(company) {
-  const comName = workCardCopy.querySelector('.work-client');
-  comName.textContent = company;
-};
-
-function setPositionName(position) {
-  const positionName = workCardCopy.querySelector('.work-role');
-  positionName.textContent = position;
-};
-
-function setWorkYear(year) {
-  const workYear = workCardCopy.querySelector('.work-year');
-  workYear.textContent = year;
-};
-
+// cards itself
 for (let i = 0; i < workCards.length; i += 1) {
-  // card copy
-  const workCardCopy = workCard1.cloneNode(true);
-  // change the title
+  let workCardCopy = workCard1.cloneNode(true);
+  // give the titles to cards
   const title = workCardCopy.querySelector('h3');
   title.innerText = workCards[i].title;
-  // make the tags
+  // set the tags
   const tag = workCardCopy.querySelector('.work-tag');
   const tagContainer = workCardCopy.querySelector('.work-tags')
-
-  setImageSrc(workCards[i].imageSrc);
   for (let j = 0; j < workCards[i].tags.length; j += 1) {
-    console.log(workCards[i].tags[j]);
     let tagCopy = tag.cloneNode(true);
     tagCopy.innerText = workCards[i].tags[j];
     tagContainer.appendChild(tagCopy);
   }
-
-  openCardBtns[i].addEventListener('click', () => {
-    workCard.classList.toggle('invisible');
-    body1.style.overflow = 'hidden';
-  });
+  tag.remove();
+  // change the information about project
+  const companyInfo = workCardCopy.querySelector('.work-client');
+  companyInfo.innerText = workCards[i].subtitle.company;
+  // change the information about job title
+  const workRole = workCardCopy.querySelector('.work-role');
+  workRole.innerText = workCards[i].subtitle.position;
+  // change the information about the year
+  const workYear = workCardCopy.querySelector('.work-year');
+  workYear.innerText = workCards[i].subtitle.year;
+  // change the images
+  function changeImg(imageSrc) {
+    const img = workCardCopy.querySelector('.work-img');
+    img.setAttribute('src', imageSrc);
+  }
+  changeImg(workCards[i].imageSrc);
+  // post the cards
   workCardContainer.appendChild(workCardCopy);
 }
 workCard1.remove();
 
-// const seeProjectBtn = workCardCopy.querySelector('.work-btn');
-// seeProjectBtn.addEventListener('click', () => {
-//   workCard.classList.toggle('invisible');
-//   const img = workCardContainer.querySelector('.work-img');
-//   img.setAttribute('src', imageSrc);
-//   body1.style.overflow = 'hidden';
-// });
-
-
 for (let i = 0; i < openCardBtn.length; i += 1) {
   openCardBtns[i].addEventListener('click', () => {
-    workCard.classList.toggle('invisible');
-    setCardTitle(workCards[i].title);
-    setImageSrc(workCards[i].imageSrc);
-    setCompanyName(workCards[i].subtitle.company);
-    setPositionName(workCards[i].subtitle.position);
-    setWorkYear(workCards[i].subtitle.year);
-    setDescription(workCards[i].description);
-    body1.style.overflow = 'hidden';
-    for (let j = 0; j < workCards[i].tags.length; i += 1) {
-      let newTag = document.createElement('li').innerHTML = `<a href="#" class="work-tag-link">${workCards[i].tags[j]}</a>`;
-      newTag.setAttribute('class', 'work-tag');
-      document.ul.appendChild(newTag);
-    }
+    workPopup.classList.toggle('invisible');
+    bodyCard.style.overflow = 'hidden';
   });
 }
 
+// card popups
 
-
-for (let i = 0; i < closeCardBtns.length; i += 1) {
-  closeCardBtns[i].addEventListener('click', () => {
-    workCard.classList.toggle('invisible');
-    body1.style.overflow = 'auto';
-  });
-}
